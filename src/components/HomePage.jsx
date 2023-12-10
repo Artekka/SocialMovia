d1db9d3dc30ecc1c4e6f11e8f5d81da575bce630
Artekka <arthur.ohumukini@gmail.com>
2023-12-09 18:42:09 -0800

    Added random placeholder images from Picsum API

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -4,8 +4,9 @@ import { useEffect } from "react"
 const Homepage = () => {
 
     const [movieCards, setMovieCards] = useState([])
+
     useEffect(() => {
-        fetch('https://jsonplaceholder.typicode.com/posts?_limit=6')
+        fetch('https://jsonplaceholder.typicode.com/posts?_limit=12')
             .then((response) => response.json())
             .then((data) => {
                 console.log(data);
@@ -16,18 +17,37 @@ const Homepage = () => {
             });
     }, []);
 
+    /* 
+    figure out Promise.all() to fetch placeholder images https://picsum.photos/v2/list?page=1&limit=12
+    */
+    
+    let multiFetch = () => {
+        Promise.all([
+            fetch('https://jsonplaceholder.typicode.com/posts?_limit=12'),
+            fetch('https://picsum.photos/v2/list?page=1&limit=12')
+        ])
+            .then((responses) => {
+                return Promise.all(responses.map(function (response) {
+                    return response.json();
+                }))
+        })
+    };
+
     return (
 
         <div id="Homepage">
+            <div className="container-fluid">
+                <div className="row">
             {movieCards.map((movieCard) => {
                 return (
-                    <div className="cardBox container-fluid" key={movieCard.id}>
+                    <div className="cardBox col-3" key={movieCard.id}>
                         <div className="cardMain">
                             <div className="cardHeader">
+                            <img src={`https://picsum.photos/200/300?random=${movieCard.id}`} alt="Super incredibly dynamic image generated via picsum's API" />
                                 <h1>{movieCard.title}</h1>
                             </div>
-                            <div className="card">
-                                <div className="cardBody"><p>
+                            <div className="cardBody">
+                                <div className=""><p>
                                     {movieCard.body}
                                 </p>
                                 </div>
@@ -38,7 +58,9 @@ const Homepage = () => {
             })
             }
 
-        </div>
+                </div>
+                </div>
+            </div>
     );
 };
 
